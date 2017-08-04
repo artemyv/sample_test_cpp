fea04947ac2e61679cfca0b19cdbe5091319128d
Artemy Vysotsky <artemyv@gmail.com>
2017-08-04 22:27:40 +0300

    Format document

--- src/test.cpp
+++ src/test.cpp
@@ -5,28 +5,28 @@
 
 class Base {
 public:
-virtual void mf1() const;
-virtual void mf2(int x);
-virtual void mf3() &;
-void mf4() const;
+    virtual void mf1() const;
+    virtual void mf2(int x);
+    virtual void mf3() &;
+    void mf4() const;
 };
 
 //compiled without warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
 class Derived: public Base {
 public:
-virtual void mf1();
-virtual void mf2(unsigned int x);
-virtual void mf3() &&;
-void mf4() const;
+    virtual void mf1();
+    virtual void mf2(unsigned int x);
+    virtual void mf3() &&;
+    void mf4() const;
 };
 
 //all 4 cases give warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
 class Derived2: public Base {
 public:
-virtual void mf1() override;
-virtual void mf2(unsigned int x) override;
-virtual void mf3() && override;
-void mf4() const override;
+    virtual void mf1() override;
+    virtual void mf2(unsigned int x) override;
+    virtual void mf3() && override;
+    void mf4() const override;
 };
 int main() {
     return 0;
