63bc614cbbfab5ee4ae9b9291c09ce9f50f68650
Artemy Vysotsky <artemyv@gmail.com>
2017-08-04 21:55:09 +0300

    Declare overriding functions override. Testing Item 12: From Effective Modern C++

--- src/test.cpp
+++ src/test.cpp
@@ -1,44 +1,33 @@
-//Testing Item 8: Prefer nullptr to 0 and NULL .
-//From Effective Modern C++
+//Declare overriding functions override.
+//Testing Item 12: From Effective Modern C++
 //  42 SPECIFIC WAYS TO IMPROVE YOUR USE OF C++11 AND C++14
 //  by Scott Meyers
 
-#include <mutex>
-#include <memory>
-#include <iostream>
-class Widget
-{
-
+class Base {
+public:
+virtual void mf1() const;
+virtual void mf2(int x);
+virtual void mf3() &;
+void mf4() const;
 };
 
-int f1(std::shared_ptr<Widget> )    // call these only when
-{
-    return 0;
-}
-double f2(std::unique_ptr<Widget> ) // the appropriate
-{
-    return 1.0;
-}
-bool f3(Widget*)                   // mutex is locked
-{
-    return true;
-}
+//compiled without warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
+class Derived: public Base {
+public:
+virtual void mf1();
+virtual void mf2(unsigned int x);
+virtual void mf3() &&;
+void mf4() const;
+};
 
-std::mutex f1m, f2m, f3m; // mutexes for f1, f2, and f3
-
-using MuxGuard = std::lock_guard<std::mutex>;
-
-template<typename FuncType, typename MuxType, typename PtrType>
-    decltype(auto) lockAndCall(FuncType func, MuxType& mutex, PtrType ptr)
-{
-    MuxGuard g(mutex);
-    return func(ptr);
-}
-
+//all 4 cases give warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
+class Derived2: public Base {
+public:
+virtual void mf1() override;
+virtual void mf2(unsigned int x) override;
+virtual void mf3() && override;
+void mf4() const override;
+};
 int main() {
-    auto result1 = lockAndCall(f1, f1m, 0); // error!
-    auto result2 = lockAndCall(f2, f2m, NULL);// error!
-    auto result3 = lockAndCall(f3, f3m, nullptr);
-    std::cout << result1 << ' ' << result2 << ' '    << result3     << std::endl;
     return 0;
 }
