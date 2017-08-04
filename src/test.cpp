f3a77059ea24360ff852673b9d2a6bb4724c9fa7
vart <artemyv@gmail.com>
2017-08-04 22:38:38 +0300

    Add comments for VS

--- src/test.cpp
+++ src/test.cpp
@@ -12,6 +12,7 @@ public:
 };
 
 //compiled without warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
+//and VS2017
 class Derived: public Base {
 public:
     virtual void mf1();
@@ -20,7 +21,8 @@ public:
     void mf4() const;
 };
 
-//all 4 cases give warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)
+//all 4 cases give warnings in gcc (g++ (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609)9)
+//and VS2017
 class Derived2: public Base {
 public:
     virtual void mf1() override;
