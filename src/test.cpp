5a3c54b77ad180be4a4e79c0a34a646da34dfc05
Artemy Vysotsky <artemyv@gmail.com>
2017-08-14 09:16:09 +0300

    Item 26/27 avoid overloading universal reference

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,52 @@
-// Item 22: When using the Pimpl Idiom, define special
-// member functions in the implementation file.
+// Item 26: Avoid overloading on universal references.
+// Item 27: Familiarize yourself with alternatives to
+//          overloading on universal references.
 //  42 SPECIFIC WAYS TO IMPROVE YOUR USE OF C++11 AND C++14
 //  by Scott Meyers
 
-#include "widget.h"
+//#include "widget.h"
+
+#include <chrono>
+#include <string>
+#include <set>
+void log (std::chrono::time_point<std::chrono::system_clock> , std::string ) {
+
+}
+
+std::string nameFromIdx(int idx)
+{
+    if(idx == 0) return "initial";
+    return "final";
+}
+
+
+template<typename T>
+void logAndAddImpl(T&& name, std::false_type)
+{
+    static std::multiset<std::string> names;
+    auto now = std::chrono::system_clock::now();
+    log(now, "logAndAdd");
+    names.emplace(std::forward<T>(name));
+}
+void logAndAddImpl(int idx, std::true_type);
+
+template<typename T>
+void logAndAdd(T&& name)
+{
+    logAndAddImpl(
+        std::forward<T>(name),
+        std::is_integral<std::remove_reference_t<T>>()
+    );
+}
+void logAndAddImpl(int idx, std::true_type)
+{
+    logAndAdd(nameFromIdx(idx));
+}
 
 int main() {
-    Widget w;
+    std::string petName("Darla");
+    logAndAdd(petName);     // pass lvalue std::string
+    logAndAdd(std::string("Persephone")); // pass rvalue std::string
+    logAndAdd("Patty Dog");
     return 0;
 }
