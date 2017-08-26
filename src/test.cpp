46cc79732e671e97c15c43aa6a608fb9279f0643
Artemy Vysotsky <artemyv@gmail.com>
2017-08-26 08:46:33 +0300

    //https://stackoverflow.com/questions/45892146/max-and-min-sum#comment78743531_45892146

--- src/test.cpp
+++ src/test.cpp
@@ -1,38 +1,44 @@
-//https://stackoverflow.com/questions/45877110/polymorphism-with-shared-ptr-of-base-class-with-3-level-inheritance
-#include <iostream>
-#include <memory>
+//https://stackoverflow.com/questions/45892146/max-and-min-sum#comment78743531_45892146
 
-class Base {
-public:
-    virtual void func() = 0;
-};
-
-
-class Derived : public Base {
-public:
-    void func() {std::cout << "Derived\n"; }
-};
-
-class DerivedDerived : public Derived {
-public:
-    void func() {std::cout << "DerivedDerived\n";} //Overrides func() from Derived
-};
-void g(std::shared_ptr<Base> s) {
-    s->func();
-}
-
+#include<iostream>
+using namespace std;
 int main()
 {
-    std::shared_ptr<Base> s1 = std::make_shared<Derived>();
-    std::shared_ptr<Base> s2 = std::make_shared<DerivedDerived>();
-    g(s1); //func() of Derived Class called
-    g(s2); //func() of Derived Class called, func() of DerivedDerived Class needed to
+    long a[5],max=0,min=10000,sum;
+    int i,j;
+    for(i=0; i<5; i++)
+        cin>>a[i];
+
+    for(j=0; j<5; j++)
+    {
+        sum=0;
+        for(i=0; i<5; i++)
+        {
+
+            if(i!=j)
+            {
+                sum=sum+a[i];
+            }
+        }
+        if(sum>max)
+            max=sum;
+        if(sum< min)
+            min=sum;
+    }
+    cout<<min<<" "<<max;
     return 0;
 }
 
-/***************
-Output:
-$ ./test
-Derived
-DerivedDerived
-***************/
+/*******************
+Answer
+
+It looks like you are having overflow issues. The max value for long type is 2,147,483,647. Adding up all the values you entered gives 3,001,208,382. Try changing
+
+long a[5], max=0, min=10000, sum; 
+
+to
+
+double a[5], max=0, min=10000, sum; 
+
+and see if you get correct values.
+*/
