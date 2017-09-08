8b6c94215a01601506b0a962e8cd0db90044b704
Artemy Vysotsky <artemyv@gmail.com>
2017-09-08 18:12:04 +0300

    add comments

--- src/test.cpp
+++ src/test.cpp
@@ -15,40 +15,60 @@ using tstring = std::wstring;
 using tstring = std::string;
 #endif
 
-tstring convert2stream(std::string source) {
-#ifdef WIN32
+//version for utf8 stdout
+template<class T>
+T convert2stream(std::string source, typename std::enable_if<std::is_same<T, std::string>::value >::type* = 0)
+{
+    return source;
+}
+
+
+//version for utf16 stdout
+template<class T>
+T convert2stream(std::string source, typename std::enable_if<std::is_same<T, std::wstring>::value >::type* = 0)
+{
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
 	return cvt.from_bytes(source);
-#else
-	return source;
-#endif
 }
 
-std::string convertFromStream(tstring source) {
-#ifdef WIN32
+//version for utf8 stdin
+template<class T>
+std::string convertFromStream(T source, typename std::enable_if<std::is_same<T, std::string>::value >::type* = 0)
+{
+    return source;
+}
+
+//version for utf16 stdin
+template<class T>
+std::string convertFromStream(T source, typename std::enable_if<std::is_same<T, std::wstring>::value >::type* = 0)
+{
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
-	return cvt.to_bytes(source);
-#else
-	return source;
-#endif
+	return cvt.from_bytes(source);
 }
-
 
 int main()
 {
 #ifdef WIN32
+	//on windows - switch to utf-16, on linus the utf-8 is default - no need to switch
 	_setmode(_fileno(stdout), _O_U16TEXT);
 	_setmode(_fileno(stdin), _O_U16TEXT);
 #endif
 
+	//internal string always utf-8
 	std::string utf8 = u8"Testing unicode -- English -- Ελληνικά -- Español.";
-	STDTCOUT << convert2stream(utf8 + '\n');
-	STDTCOUT << convert2stream("Enter new string: ");
 
+	//when writing to stdout - optionally convert
+	STDTCOUT << convert2stream<tstring>(utf8 + '\n');
+	STDTCOUT << convert2stream<tstring>("Enter new string: ");
+
+	//when reading from stdin - use intemediate type
 	tstring ws;
 	getline(STDTCIN, ws);
 
+	//optionally convert to inernal utf8 representation
 	utf8 = convertFromStream(ws);
-	STDTCOUT << convert2stream(utf8 + '\n');
+
+	//check what is being read
+	STDTCOUT << convert2stream<tstring>(utf8 + '\n');
 
 }
