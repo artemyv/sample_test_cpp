20abc2a9309745b2070e44145cfbb7635cc4efbc
Artemy Vysotsky <artemyv@gmail.com>
2017-09-20 12:53:59 +0300

    Fix subsctr

--- src/widget.cpp
+++ src/widget.cpp
@@ -1,32 +1,33 @@
 //https://stackoverflow.com/questions/46317751/utf-8-sprintf-strlen-etc#46318241
 
-#include <iostream>
-#include <codecvt>
-#include <string>
-#include <locale>
+    #include <iostream>
+    #include <codecvt>
+    #include <string>
+    #include <locale>
 
-std::string cutString(const std::string& in, size_t len)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
-    auto wstring = cvt.from_bytes(in);
-    if(len < wstring.length())
+    std::string cutString(const std::string& in, size_t len)
     {
-        wstring = wstring.substr(len);
-        return cvt.to_bytes(wstring);
-    }    
-    return in;
-}
-int main(){
-    std::string test = "你好世界這是演示樣本";
+        std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
+        auto wstring = cvt.from_bytes(in);
+        if(len < wstring.length())
+        {
+            wstring = wstring.substr(0,len);
+            return cvt.to_bytes(wstring);
+        }    
+        return in;
+    }
+    int main(){
+        std::string test = "你好世界這是演示樣本";
 
-    std::string res = cutString(test,5);
-    std::cout << res << '\n';
-    
-    return 0;
-}
+        std::string res = cutString(test,5);
+        std::cout << test << '\n' << res << '\n';
+        
+        return 0;
+    }
 
-/****************
-Output 
-$ ./test
-是演示樣本
-*/
+    /****************
+    Output 
+    $ ./test
+    你好世界這是演示樣本
+    你好世界這
+    */
