5bc5fdd9a7b0d3c5ae99f6c16aa9d40f942b77a4
Artemy Vysotsky <artemyv@gmail.com>
2018-07-27 08:15:01 +0300

    Testing enabled_if on class

--- Makefile
+++ Makefile
@@ -1,15 +1,12 @@
 IDIR = include
-IDIR2 = ../jsoncpp/include
 
 SDIR = src
-SDIR2 = ../jsoncpp/src/lib_json
 ODIR = obj
-ODIR2 = obj2
 LDIR = lib
 
-CC=clang++-4.0
+#CC=clang++-4.0
 #CC=g++
-#CC=g++-7
+CC=g++-8
 
 V = 0
 ACTUAL_CC := $(CC)
@@ -17,28 +14,15 @@ CC_0 = @echo "Compiling $^ -> $@"; $(ACTUAL_CC)
 CC_1 = $(ACTUAL_CC)
 CC = $(CC_$(V))
 
+#-v
+CFLAGS=  -g -std=c++17  -Werror -Wall -Wextra -pedantic -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -I$(IDIR)
 
-#CFLAGS=-Wall -Wextra -Werror -Wuninitialized -pedantic -std=c++14 -I$(IDIR)
 
-#CFLAGS= -g -std=c++14 -Werror -Wall -Wextra -pedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -I$(IDIR)
-ifeq ($(CC),g++)
-	CFLAGS= -v -g -std=c++14 -DBOOST_LOG_DYN_LINK -Werror -Wall -Wextra -pedantic -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option -I$(IDIR) -I$(IDIR2)
-else
-	CFLAGS= -g -std=c++1z -DBOOST_LOG_DYN_LINK -Werror -Wall -Wextra -pedantic -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-overflow=5 -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option  -I$(IDIR) -I$(IDIR2)
-endif
-#CFLAGS= -g -std=c++14 -Wall -fexceptions -fPIC  -I$(IDIR) -I$(IDIR2) -Wno-unknown-pragmas -Wno-deprecated-declarations
-
-LIBS=-lm -lpthread -lboost_log -lboost_log_setup  -lboost_system -lboost_thread 
-
-#_DEPS = hellomake.h
 DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
 
-#_OBJ = hellomake.o hellofunc.o
 CPP_FILES1 := $(wildcard $(SDIR)/*.cpp)
-CPP_FILES2 := $(wildcard $(SDIR2)/*.cpp)
 
 OBJ_FILES1 := $(addprefix $(ODIR)/,$(notdir $(CPP_FILES1:.cpp=.o)))
-OBJ_FILES2 := $(addprefix $(ODIR2)/,$(notdir $(CPP_FILES2:.cpp=.o)))
 
 
 MKDIR_P = mkdir -p
@@ -47,23 +31,16 @@ MKDIR_P = mkdir -p
 
 all: directories test
 
-directories: ${ODIR} ${ODIR2}
+directories: ${ODIR}
 
 ${ODIR}:
 	${MKDIR_P} ${ODIR}
-${ODIR2}:
-	${MKDIR_P} ${ODIR2}
 
-
-test: $(OBJ_FILES1) $(OBJ_FILES2)
+test: $(OBJ_FILES1) 
 	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
 
 $(ODIR)/%.o: $(SDIR)/%.cpp $(DEPS)
 	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
-
-$(ODIR2)/%.o: $(SDIR2)/%.cpp $(DEPS)
-	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
-
 
 .PHONY: clean
 
--- src/test.cpp
+++ src/test.cpp
@@ -1,135 +1,45 @@
 ﻿#include <iostream>
-#include <codecvt>
-#include <locale>
-#include <type_traits>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <mutex>
 
-#ifdef WIN32
-#include <io.h>
-#include <fcntl.h>
-#endif
-
-class MyCout
+template<class T, class Enable = void>
+struct RealNumber
 {
-private:
-	static std::once_flag flag1;
-	bool bWide = false;
-public:
-	MyCout()
-	{
-		std::call_once(flag1, [](){ 
-#ifdef WIN32
-			//on windows - switch to utf-16, on linus the utf-8 is default - no need to switch
-			_setmode(_fileno(stdout), MODETEXT);
-			_setmode(_fileno(stdin), MODETEXT);
-#endif
-				});
-#ifdef WIN32
-		bWide = true;
-#endif
-	}
-	std::stringstream s;
+	RealNumber(double val)
+		: integer(static_cast<int>(val)),
+		  fraction(static_cast<short>((val - integer)*100))
+	{}
 
-	template <typename T>
-	MyCout& operator << (const T &x) {
-		s << x;
-		return *this;
-	}
-
-	~MyCout() {
-		if(bWide){
-			std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
-			std::wcout << cvt.from_bytes( s.str());
-		}
-		else{
-			std::cout << s.str();
-		}
-	}
+	int integer;
+	short fraction;
 };
 
-//code below is the same for linux/windows
-//linux part works out-of-the-box
-//windows version requires following pre-sets
-// 1. Store the source cpp file as UTF-8 BOM format before ccompiling
-// 2. Change the font in the command line to one suporting unicode gliphs
-//    Following answer worked for me https://stackoverflow.com/a/38694885/8491726
-
-//version for utf8 stdout
 template<class T>
-T convert2stream(std::string source, typename std::enable_if<std::is_same<T, std::string>::value >::type* = 0)
+struct RealNumber<T, typename std::enable_if_t<std::is_floating_point<T>::value>>
 {
-	return source;
-}
+	RealNumber(T val)
+		: value(val)
+	{}
 
-//version for utf16 stdout
-template<class T>
-T convert2stream(std::string source, typename std::enable_if<std::is_same<T, std::wstring>::value >::type* = 0)
-{
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
-	return cvt.from_bytes(source);
-}
+	T value;
+};
 
-//version for utf8 stdin
-template<class T>
-std::string convertFromStream(T source, typename std::enable_if<std::is_same<T, std::string>::value >::type* = 0)
-{
-	return source;
-}
-
-//version for utf16 stdin
-template<class T>
-std::string convertFromStream(T source, typename std::enable_if<std::is_same<T, std::wstring>::value >::type* = 0)
-{
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> cvt;
-	return cvt.to_bytes(source);
-}
-#if 0
 int main()
 {
-	//internal string always utf-8
-	std::string utf8 = u8"Testing unicode -- English -- 水 -- Ελληνικά -- Español -- Русский -- עיברית.";
 
-	//when writing to stdout - optionally convert
-	MyCout() << utf8  << '\n';
-	MyCout() << "Enter new string: ";
+	auto a = RealNumber<double>(1.3);
+	auto b = RealNumber<float>(1.3f);
+	auto c = RealNumber<long double>(1.3l);
+	auto d = RealNumber<int>(1);
+	auto e = RealNumber<int>(1.5);
 
-	MyCout() << "test 1" << 1 << std::endl; 
-	#if 0
-	//when reading from stdin - use intemediate type
-	tstring ws;
-	getline(STDTCIN, ws);
+	std::cout << a.value << std::endl;
+	std::cout << b.value << std::endl;
+	std::cout << c.value << std::endl;
+	std::cout << d.integer << '.' << d.fraction << std::endl;
+	std::cout << e.integer << '.' << e.fraction << std::endl;
 
-	//optionally convert to inernal utf8 representation
-	std::string user = convertFromStream(ws);
-
-	//check what is being read
-	STDTCOUT << convert2stream<tstring>(user + '\n');
-
-	std::ofstream f1("test_utf8_direct.txt");
-
-	f1 << utf8 << '\n' << user << '\n';
-	f1.close();
-
-	std::wofstream f2;
-	std::locale l1;
-	std::locale l2(l1, new std::codecvt_utf8<wchar_t>);
-	f2.imbue(l2);
-	f2.open("test_utf8_from16.txt");
-	f2 << convert2stream<std::wstring>(utf8 + '\n');
-	f2 << convert2stream<std::wstring>(user + '\n');
-	#endif
+	return 0;
 }
-/*************
-Output
-$ ./test
-Testing unicode -- English -- 水 -- Ελληνικά -- Español -- Русский -- עיברית.
-Enter new string: hello привет שלום
-hello привет שלום
-
-
-Actually hebrew text in the console is show left to right direction instad of correct right to left 
-*/
-#endif
